5efd41fcc270d1ccc4407f1fbfbd618f6c4aaeef
Luidson Lima Santos <luidsonlsti@gmail.com>
2024-04-09 20:46:52 -0300

    primeira interface

---
+++ gui/hello.py
@@ -0,0 +1,63 @@
+import kivy
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+
+
+class NumericInput(TextInput):
+    def insert_text(self, substring, from_undo=False):
+        if substring.isdigit() or substring == "":
+            return super().insert_text(substring, from_undo=from_undo)
+        else:
+            return False
+
+class MainGridLayout(GridLayout):
+
+
+    def __init__(self, **kwargs):
+        super(MainGridLayout, self).__init__(**kwargs)
+
+        self.name_label = Label(text="Qual seu nome?")
+        self.add_widget(self.name_label)
+        
+        self.name_input = TextInput(multiline = False)
+        self.add_widget(self.name_input)
+
+        self.birth_label = Label(text="Em qual ano você nasceu?")
+        self.add_widget(self.birth_label)
+
+        self.birth_input = NumericInput(multiline = False)
+        self.add_widget(self.birth_input)
+
+        self.submit_button = Button(text="Enviar", font_size=32)
+        self.submit_button.bind(on_press=self.press)
+        self.add_widget(self.submit_button)
+
+        self.text_message = Label()
+        self.add_widget(self.text_message)
+
+        self.cols = 2
+
+
+    def press(self, instance):
+        name = self.name_input.text
+        birth = self.birth_input.text
+
+        if name == '' or birth == '':
+            return
+        else:
+            if int(birth)> 1999:
+                self.text_message.text = f"{name}, infelizmente você é NUTELLA :("
+            else:
+                self.text_message.text = f"Parabéns, {name}, você não é nutella"
+        
+
+class MyApp(App):
+    def build(self):
+        return MainGridLayout()
+    
+
+if __name__ == '__main__':
+    MyApp().run()
