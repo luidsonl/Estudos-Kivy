f294bb5d00ee9e7987a9a5997ede61152d597059
Luidson Lima Santos <luidsonlsti@gmail.com>
2024-07-19 23:13:25 -0300

    calculadora - melhora interface e implementa lógica de validação de input

--- calculator/widgets/Calc.py
+++ calculator/widgets/Calc.py
@@ -1,10 +1,78 @@
-from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
+import re
 
 
 class Calc(BoxLayout):
     Builder.load_file('./screens/Calc.kv')
 
+    register = ''
+
+    def update_input(self):
+        self.ids.calc_input.text = self.register
+
+    def update_register(self, text):
+        self.register = text
+
+    def sanitize_register(self):
+        sanitized_register = []
+
+        for i, char in enumerate(self.register):
+            if i == 0:
+                if char in ['0','1','2','3','4','5','6','7','8','9','-']:
+                    sanitized_register += char
+                continue
+
+            if char in['0','1','2','3','4','5','6','7','8','9']:
+                if sanitized_register[-1] =='%':
+                    continue
+                sanitized_register += char
+                continue
+
+            if char in ['.', '%']:
+                flag = True
+                for i, s_char in enumerate(sanitized_register):
+                    if s_char in ['.','/','*','-','+', '%']:
+                        flag = False
+                    if s_char in ['0','1','2','3','4','5','6','7','8','9']:
+                        if sanitized_register[i-1] in ['/','*','-','+']:
+                            flag = True
+
+                if flag:
+                    sanitized_register += char
+                    continue
+
+
+            if (sanitized_register[-1] in ['/','*','-','+', '.'] and char in ['/','*','-','+']):
+                sanitized_register[-1] = char
+                continue
+            
+            if char in ['/','*','-','+']:
+                sanitized_register += char
+                continue
+        
+        self.register = ''.join(sanitized_register)
+            
+
+
+    def validate_input(self, instance, text):
+            self.update_register(text)
+            self.sanitize_register()
+            self.update_input()
+
+
     def clear(self):
-        self.ids.calc_input.text = '0'
+        self.register = ''
+        self.update_input()
+
+    def back(self):
+        self.register = self.register[:-1]
+        self.update_input()
+
+    def button_click(self, value):
+        if value == 'X':
+            value = '*'
+        
+        self.register += value
+        self.sanitize_register()
+        self.update_input()
--- calculator/widgets/CalcKey.py
+++
@@ -1,9 +0,0 @@
-from kivy.uix.button import Button
-
-class CalcKey(Button):
-     def __init__(self, **kwargs):
-
-        super().__init__(**kwargs)
-
-        self.size_hint = (0.2 ,0.2)
-        self.font_size = 32
